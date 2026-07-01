0c3c9494ac5744b368b586ddc48409ec6f7e978e
Dmytro Lenivenko <dmytro.lenivenko@omnibees.com>
2026-07-01 09:53:59 +0100

    1st controller

---
+++ StayPilot/StayPilot.Api/Controllers/MarketAreasController .cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StayPilot.Application.Contracts.Response;
+using StayPilot.Application.Interfaces;
+
+namespace StayPilot.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MarketAreasController : ControllerBase
+    {
+        private readonly IMarketAreaService _service;
+
+        public MarketAreasController(IMarketAreaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<MarketAreaResponse>>> GetAll()
+        {
+            var response = await _service.GetAllMarketAreasAsync();
+            return Ok(response);
+        }
+    }
+}
