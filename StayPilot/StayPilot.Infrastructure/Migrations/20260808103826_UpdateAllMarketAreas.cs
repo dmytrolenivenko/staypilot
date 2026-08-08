218842e383157dcee9c19991320c55824c2df51c
Dmytro Lenivenko <dmytro.lenivenko@omnibees.com>
2026-08-08 11:41:04 +0100

    Updated MarketAreas

---
+++ StayPilot/StayPilot.Infrastructure/Migrations/20260808103826_UpdateAllMarketAreas.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StayPilot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAllMarketAreas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3406,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Faro", "Loulé" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3413,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Faro", "Loulé" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3414,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Faro", "Loulé" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3419,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Faro", "Loulé" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3423,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Faro", "Loulé" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3424,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Faro", "Loulé" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3406,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Setúbal", "Setúbal" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3413,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Setúbal", "Setúbal" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3414,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Setúbal", "Setúbal" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3419,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Setúbal", "Setúbal" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3423,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Setúbal", "Setúbal" });
+
+            migrationBuilder.UpdateData(
+                table: "MarketAreas",
+                keyColumn: "Id",
+                keyValue: 3424,
+                columns: new[] { "District", "Municipality" },
+                values: new object[] { "Setúbal", "Setúbal" });
+        }
+    }
+}
